bea13f1a1cff4cbebb24d8714a4629c6f935829b
CintiaMariana <81639655+CintiaMariana@users.noreply.github.com>
2024-05-18 13:48:01 -0300

    Delete deliveries directory

--- deliveries/src/main/java/br/edu/univas/deliveries/suporte/DeliveriesException.java
+++
@@ -1,9 +0,0 @@
-package br.edu.univas.deliveries.suporte;
-
-public class DeliveriesException extends RuntimeException{
-	
-	public DeliveriesException(String message) {
-		super(message);
-	}
-
-}
--- deliveries/src/main/java/br/edu/univas/deliveries/suporte/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.edu.univas.deliveries.suporte;
-
-public class ObjectNotFoundException extends RuntimeException {
-	
-	public ObjectNotFoundException(String message) {
-		super(message);
-	}
-
-}
